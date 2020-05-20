0a0459193f1fa7e18d6d9bcd251572923e072062
liheng <liheng@globalegrow.com>
2020-05-20 18:18:55 +0800

    静态页面优化

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,8 +6,13 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>@yield('title', 'August 5th')</title>
+    <link rel="stylesheet" href="/css/font.css">
+    <link rel="stylesheet" href="/css/app.css">
+    <script src="/js/app.js"></script>
 </head>
-<body>
-@yield('content')
+<body class="antialiased font-sans bg-gray-200">
+<div class="">
+    @yield('content')
+</div>
 </body>
 </html>
